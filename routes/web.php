8bbfbc19ec2a7be4fcb752248d19aa9edbd8c830
Ry0xi <ryoki0525tnk@gmail.com>
2021-12-03 00:01:58 +0900

    Episode 58 Complete

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us20'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('c93fb4f50f', [
+        'emaail_address' => '',
+        'status' => 'subscribed'
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
