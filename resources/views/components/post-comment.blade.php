da03caf0c534f20ad894ef9d3ae6b1e3750eb588
Ry0xi <ryoki0525tnk@gmail.com>
2021-12-02 13:57:12 +0900

    Episode 52 Complete

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3>Jone Doe</h3>
+            <p class="text-sm">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque animi vel quis tempore autem eum quasi odit perspiciatis magnam. Explicabo a sapiente alias vero ipsum minus libero rem unde obcaecati.</p>
+    </div>
+</article>
