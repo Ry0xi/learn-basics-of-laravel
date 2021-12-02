baf9b315e98145265f5b42cc892e8d9ed6638f00
Ry0xi <ryoki0525tnk@gmail.com>
2021-12-02 15:21:57 +0900

    Episode 54 Complete

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,19 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3>Jone Doe</h3>
-            <p class="text-sm">
+            <h3>{{ $comment->author->username }}</h3>
+            <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque animi vel quis tempore autem eum quasi odit perspiciatis magnam. Explicabo a sapiente alias vero ipsum minus libero rem unde obcaecati.</p>
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
